e75563a4847e9594824cb6a9df9f394010e39ac7
caltabianomusic <caltabiano@live.com>
2025-06-16 18:15:20 +0200

    Add files via upload

---
+++ tracks.js
@@ -0,0 +1,42 @@
+//* Track
+
+const tracks = [
+  //* {
+  //*   artwork:
+  //*    "https://cdn.pixabay.com/audio/2022/05/27/23-46-21-714_200x200.jpg",
+  //*   name: "Best Time",
+  //*    artist: "FAS Sounds",
+  //*   path: "./media/112194.mp3",
+  //*  },
+
+  {
+    artwork: "./images/magdiel2.jpg",
+    name: "Estar",
+    artist: "Caltabiano",
+    path: "./media/estar.mp3",
+  },
+  {
+    artwork: "./images/magdiel2.jpg",
+    name: "La Vida Que LLevo",
+    artist: "Caltabiano",
+    path: "./media/LaVidaQueLLevo.mp3",
+  },
+  {
+    artwork: "./images/magdiel2.jpg",
+    name: "Nadie",
+    artist: "Caltabiano",
+    path: "./media/nadie.mp3",
+  },
+  {
+    artwork: "./images/magdiel2.jpg",
+    name: "No hay otra opción",
+    artist: "Caltabiano",
+    path: "./media/NoHayOtraOpción.mp3",
+  },
+  {
+    artwork: "./images/magdiel2.jpg",
+    name: "Ruptura",
+    artist: "Caltabiano",
+    path: "./media/Ruptura.mp3",
+  },
+];
